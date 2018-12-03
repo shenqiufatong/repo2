855942482c8e5180d88383af9c9d825e6e3ddc7d
xtf <xtf@qq.com>
2018-12-03 18:16:04 +0800

    repo2修改啦

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+ System.out.println("HelloWorld1!");
+ System.out.println("HelloWorld2!");
+ System.out.println("HelloWorld!3");
+ System.out.println("HelloWorld4");
     }
 }
